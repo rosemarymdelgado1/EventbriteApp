04ff8aff7f022fab94747041ed3aad212bee4565
Rosemary Delgado <33888379+rosemarymdelgado1@users.noreply.github.com>
2020-03-30 02:57:54 -0700

    Added EventCategory, EventItem and EventLocation classes

--- ServiceCatalogApi/Controllers/CatalogController.cs
+++ ServiceCatalogApi/Controllers/CatalogController.cs
@@ -9,7 +9,7 @@ namespace ServiceCatalogApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EventController : ControllerBase
+    public class CatalogController : ControllerBase
     {
     }
 }
--- ServiceCatalogApi/Domain/EventItem.cs
+++ ServiceCatalogApi/Domain/EventItem.cs
@@ -7,5 +7,16 @@ namespace ServiceCatalogApi.Domain
 {
     public class EventItem
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public string Organizer { get; set; }
+        public string PictureUrl { get; set; }
+        public int EventCategoryId { get; set; }
+        public int EventLocationId { get; set; }
+        public virtual EventCategory EventCategory { get; set; }
+        public virtual EventLocation EventLocation { get; set; }
     }
 }
