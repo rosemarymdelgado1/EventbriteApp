af35fdc826819d0dd10bb4b9f57d9fd0c58a0e00
Rosemary Delgado <33888379+rosemarymdelgado1@users.noreply.github.com>
2020-03-30 02:15:06 -0700

    Initial commit

---
+++ ServiceCatalogApi/Domain/EventType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceCatalogApi.Domain
+{
+    public class EventType
+    {
+    }
+}
