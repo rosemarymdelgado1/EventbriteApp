cefffe9b86dd2d028b3038e8a4eaf28fd06c3722
Rosemary Delgado <33888379+rosemarymdelgado1@users.noreply.github.com>
2020-04-19 12:54:15 -0700

    Addint Infrastructure + Models

---
+++ WebMVC/Infrastructure/ApiPaths.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static class Catalog
+        {
+            public static string GetAllEventItems(string baseUri,
+                int page, int take, int category, int type)
+            {
+                var filterQs = string.Empty;
+                if (category != 0 || type != 0)
+                {
+                    var categoryQs = (category != 0) ? "eventTypeId=" + category : "";
+                    var typeQs = (type != 0) ? "eventCategoryId=" + type : "";
+                    filterQs = $"{typeQs}&{categoryQs}";
+                }
+                return $"{baseUri}items?{filterQs}&pageIndex={page}&pageSize={take}";
+            }
+
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}eventtypes";
+            }
+            public static string GetAllCategories(string baseUri)
+            {
+                return $"{baseUri}eventcategories";
+            }
+        }
+        public static class Basket
+        {
+
+        }
+    }
+}
---
+++ WebMVC/Models/EventItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMVC.Models
+{
+    public class EventItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public string Venue { get; set; }
+        public string Organizer { get; set; }
+        public string PictureUrl { get; set; }
+        public int EventCategoryId { get; set; }
+        public int EventTypeId { get; set; }
+        public int EventLocationId { get; set; }
+        public string EventCategory { get; set; }
+        public string EventType { get; set; }
+        public string EventLocation { get; set; }
+    }
+}
